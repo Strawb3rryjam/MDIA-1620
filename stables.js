b399cb3e200b5bde0912c450afbe4536e317c30a
StrawB3rryjam <Vicentee.jamille@gmail.com>
2024-09-19 15:48:20 -0700

    initalize stables and add a horse

---
+++ stables.js
@@ -0,0 +1,20 @@
+
+console.log("horse are great!");
+
+let horseName = "Jerry"; 
+
+horseName ="Charlie";
+
+let horseAge = 5;
+
+//boolean
+let isHorseInStable = true; 
+
+console.log(horseName);
+console.log(horseAge);
+
+console.log(isHorseInStable);
+
+/**YEEE HAWW
+ * tee hee
+ */
