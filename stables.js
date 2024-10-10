24b35c9cd3782712520f86b11a4305a09f426be0
StrawB3rryjam <Vicentee.jamille@gmail.com>
2024-10-09 23:57:55 -0700

    update lesson 5

--- stables.js
+++ stables.js
@@ -9,3 +9,6 @@ const STABLE_MONTH_FEE = 100;
 let saved_MONEY = (100 * 3)/10;
 
 console.log(`You've saved $${saved_MONEY} with a 10% discount applied with your 3 month stay at the stables.`);
+
+// LESSON-5 Code
+console.log(`${nameHorse} is outside galloping freely`); 
