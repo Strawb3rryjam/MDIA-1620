c168edf1c445ca186aa7f2969cfed8cb84e62279
StrawB3rryjam <Vicentee.jamille@gmail.com>
2024-10-10 19:04:13 -0700

    lab 5 updated

--- stables.js
+++ stables.js
@@ -1,14 +1,22 @@
 /**horse*/
 let nameHorse = "Caitlyn";
 let horseName2 = "Cait";
-console.log(`This is my horse ${nameHorse} and her nickname is ${horseName2}.`);
+//console.log(`This is my horse ${nameHorse} and her nickname is ${horseName2}.`);
 
 
 /**fees */
 const STABLE_MONTH_FEE = 100;
 let saved_MONEY = (100 * 3)/10;
 
-console.log(`You've saved $${saved_MONEY} with a 10% discount applied with your 3 month stay at the stables.`);
+//console.log(`You've saved $${saved_MONEY} with a 10% discount applied with your 3 month stay at the stables.`);
 
 // LESSON-5 Code
-console.log(`${nameHorse} is outside galloping freely`); 
+
+isHorseOutside = false;
+
+
+if (isHorseOutside === true ){
+    console.log(`${nameHorse} is outside galloping freely!`); 
+} else {
+    console.log(`${nameHorse} is inside eating hay.`);
+}
